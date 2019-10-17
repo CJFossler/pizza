c50df8423b39caab532e8394a0e23dc3f543f68b
CJFossler <56641005+CJFossler@users.noreply.github.com>
2019-10-16 21:10:04 -0400

    Add files via upload

---
+++ pizzaFunctions.js
@@ -0,0 +1,12 @@
+/*
+ * A set of functions to change he bg color
+ */
+
+// Changed the bg to red
+function setRed(){
+	document.body.style.background = "red";
+}
+// Changed the bg to blue
+function setWhite(){
+	document.body.style.background = "wheat";
+}
